cf583f80a3d164cdcc5070d9d5ee1fe5cbe8db93
Miguel Olarte <mbsic91@gmail.com>
2025-04-08 11:49:49 -0500

    feat: Se crea la clase CitaService y se le agregan los métodos abstractos a CitaRepository, para generar consultas personalizadas

--- src/main/java/com/katusoft/barberstown/repository/CitaRepository.java
+++ src/main/java/com/katusoft/barberstown/repository/CitaRepository.java
@@ -1,9 +1,20 @@
 package com.katusoft.barberstown.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.katusoft.barberstown.enums.EstadoCita;
 import com.katusoft.barberstown.model.Cita;
+import java.time.LocalDate;
+
 
 public interface CitaRepository extends JpaRepository<Cita, Long>{
+
+    List<Cita> findByBarberoId(Long barberoId);
+    List<Cita> findByClienteId(Long clienteId);
+    List<Cita> findByFecha(LocalDate fecha);
+    List<Cita> findBybarberoIdAndFecha(Long id, LocalDate fecha);
+    List<Cita> findByEstado(EstadoCita estado);
 
 }
---
+++ src/main/java/com/katusoft/barberstown/service/CitaService.java
@@ -0,0 +1,8 @@
+package com.katusoft.barberstown.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CitaService {
+
+}
